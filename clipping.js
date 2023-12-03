0a3eb86c043aeca9a3c8ec011f2cb8a27f7b3610
benWhat <77498601+benWhat@users.noreply.github.com>
2023-12-03 18:47:15 +0000

    added error handling WIP

--- clipping.js
+++ clipping.js
@@ -1,54 +1,89 @@
 const puppeteer = require('puppeteer');
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
+const cfg = require('./config');
 
-async function captureVideo(captureId, videoUrl) {
-  const captureData = captureQueue.find((item) => item.captureId === captureId);
-  captureData.status = 'in-progress';
-  captureData.message = `Video capture #${captureId} is in progress.`;
+const ffmpegPath = cfg.gConfig.ffmpegPath;
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const videoOutputFolder = cfg.gConfig.videoOutputFolder;
+
+async function handleConsentPopup(page) {
+  // Customize this function based on the structure of the consent pop-up on the specific website
+
+  // Example: Clicking on a button with id "acceptButton" for consent
+  await page.click('button:contains("Accept All")');
+
+  // You may need to wait for the page to reload or for the consent pop-up to disappear
+  await page.waitForTimeout(2000);  // Adjust the timeout as needed
+}
+
+async function captureVideo(captureId, videoUrl, captureQueue) {
+  const captureData = { captureId, status: 'pending', message: '' }; // Replace this with your capture data structure
+  captureQueue.push(captureData);
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   try {
-
     await page.goto(videoUrl, { waitUntil: 'domcontentloaded' });
+    // Handle consent pop-up (customize this based on your specific case)
+    await handleConsentPopup(page);
     await page.waitForSelector('video');
 
     await page.evaluate(() => {
       document.querySelector('video').play();
     });
 
-    // clip 60 seconds of video
-    await new Promise(resolve => setTimeout(resolve, 60000));
-
-    await page.evaluate(() => {
-      document.querySelector('video').pause();
-    });
-
-    for (let i = 0; i < 30 * 30; i++) {
-      const screenshotPath = path.join(__dirname, 'public', `screenshot_${i + 1}.png`);
+    // Capture for 30 seconds, taking a screenshot every second
+    for (let i = 0; i < 30; i++) {
+      const screenshotPath = path.join(videoOutputFolder, `screenshot_${i + 1}.png`);
       await page.screenshot({ path: screenshotPath });
-      await page.waitForTimeout(1000 / 30);
+
+      // Use setTimeout to introduce a delay
+      await new Promise(resolve => setTimeout(resolve, 1000));
     }
   } finally {
-    await browser.close();
-    captureData.status = 'completed';
-    captureData.message = `Video capture #${captureId} is completed.`;
+    try {
+      await browser.close();
+      captureData.status = 'completed';
+      captureData.message = `Video capture #${captureId} is completed.`;
 
-    // Constructing file paths using path.join for consistency
-    const screenshotFolder = path.join(__dirname, 'public');
-    const screenshotPattern = path.join(screenshotFolder, `screenshot_%d.png`);
-    const videoOutputPath = path.join(screenshotFolder, `output_${captureId}.mp4`);
+      const screenshotPattern = path.join(videoOutputFolder, 'screenshot_%d.png');
+      const videoOutputPath = path.join(videoOutputFolder, `output_${captureId}.mp4`);
 
-    // Move video encoding logic here if you want to process the video immediately
-    ffmpeg()
-      .input(screenshotPattern)
-      .inputFPS(30)
-      .output(videoOutputPath)
-      .on('end', () => console.log(`Video encoding for ${captureId} complete`))
-      .run();
+      // encode video
+      await new Promise((resolve, reject) => {
+        ffmpeg()
+          .input(screenshotPattern)
+          .inputFPS(30)
+          .videoCodec('libx264')
+          .outputOptions('-pix_fmt yuv1080p')
+          .output(videoOutputPath)
+          .on('end', () => {
+            console.log(`Video encoding for ${captureId} complete`);
+            resolve();
+          })
+          .on('error', (err) => {
+            console.error(`Error encoding video: ${err.message}`);
+            console.error(err);
+            reject(err);
+          })
+          .on('progress', (progress) => {
+            console.log(`Processing: ${progress.percent}% done`);
+          })
+          .run();
+      });
+    } catch (err) {
+      console.error(`Error during video encoding: ${err.message}`);
+      captureData.status = 'failed';
+      captureData.message = `Video capture #${captureId} failed during encoding.`;
+      return err;
+    }
   }
 }
 
 module.exports = captureVideo;
+// Example usage
+// const captureQueue = [];
+// captureVideo(1, 'https://www.youtube.com/watch?v=gb2jgHpbowY');  // Replace with the actual video URL
--- server.js
+++ server.js
@@ -1,36 +1,45 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const captureVideo = require('./clipping');
+const cfg = require('./config');
 
 const app = express();
-const PORT = 3000;
+const PORT = cfg.gConfig.port;
 
 const captureQueue = [];
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.post('/capture-videos', async (req, res) => {
   const videoUrls = req.body.videoUrls;
 
-  const captureIds = [];
-  for (const url of videoUrls) {
-    const captureId = captureQueue.length + 1;
-    captureIds.push(captureId);
+  try {
+    if (!Array.isArray(videoUrls) || videoUrls.length === 0) {
+      return res.status(400).send('Invalid video URLs');
+    }
+    const captureIds = [];
+    for (const url of videoUrls) {
+      const captureId = captureQueue.length + 1;
+      captureIds.push(captureId);
 
-    captureQueue.push({
-      captureId,
-      status: 'pending',
-      message: `Video capture #${captureId} for ${url} is pending.`,
-    });
-    captureVideo(captureId, url);
+      captureQueue.push({
+        captureId,
+        status: 'pending',
+        message: `Video capture #${captureId} for ${url} is pending.`,
+      });
+     await captureVideo(captureId, url, captureQueue);
+    }
+    res.status(200).json({ captureIds, message: 'Video capture complete' });
+
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error capturing videos')
   }
-
-  res.send({ captureIds, message: 'Video capture initiated. Check the "public" folder for the output videos.' });
 });
 
 app.listen(PORT, () => {
